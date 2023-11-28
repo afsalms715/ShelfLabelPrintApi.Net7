dd9b77bafaed09b4bb74a50fe74dd4957bdf6f84
afsalms715 <afsalms715@gmail.com>
2023-11-28 19:05:45 +0300

    Add project files.

---
+++ ShelfLabelPrint/Controllers/ProductDetailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShelfLabelPrint.Services;
+
+namespace ShelfLabelPrint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailController : ControllerBase
+    {
+        ProductDtlService productDtlService;
+        public ProductDetailController(ProductDtlService productDtlService)
+        {
+            this.productDtlService = productDtlService;
+        }
+        [HttpGet("GetProductDtl")]
+        public string GetProductDtl()
+        {
+            string connectionString = productDtlService.GetConnectioinString();
+            return connectionString;
+        }
+    }
+}
---
+++ ShelfLabelPrint/Services/ProductDtlService.cs
@@ -0,0 +1,14 @@
+﻿namespace ShelfLabelPrint.Services
+{
+    public class ProductDtlService
+    {
+        public string connectionString { get; set; }
+        public ProductDtlService(string connectionString) { 
+            this.connectionString = connectionString;
+        }
+        public string GetConnectioinString()
+        {
+            return connectionString;
+        }
+    }
+}
